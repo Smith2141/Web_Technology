db5556885894be553184db0d86eba046e7a5c661
smith_mint <berez_max@mail.ru>
2019-11-11 22:56:10 +0300

    WSGI 3

--- init.sh
+++ init.sh
@@ -3,4 +3,4 @@ sudo rm /etc/nginx/sites-enabled/default 2> /dev/null
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
-sudo /etc/init.d/mysql start
+#sudo /etc/init.d/mysql start
