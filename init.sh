4ad7a735fb347622e8b8792e1879b6354137550c
smith_mint <berez_max@mail.ru>
2019-11-11 23:12:48 +0300

    WSGI 4

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo rm /etc/nginx/sites-enabled/default 2> /dev/null
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test.conf
 sudo /etc/init.d/gunicorn restart
 #sudo /etc/init.d/mysql start
