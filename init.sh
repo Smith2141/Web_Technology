89745308084f58d1be8634367c58fdd991ff78fa
smith_mint <berez_max@mail.ru>
2019-11-13 00:09:24 +0300

    WSGI 10

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+#sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-available/default
 #sudo rm /etc/nginx/sites-enabled/default 2> /dev/null
 sudo rm /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
