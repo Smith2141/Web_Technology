2d9e6ea452daf6cf616bcb863d81d1e0b7bb866e
smith_mint <berez_max@mail.ru>
2019-11-12 23:29:58 +0300

    WSGI 9

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 def wsgi_application(environ, start_response):
     status = '200 OK'
     headers = [('Content-Type', 'text/plain')]
-    result = [bytes(i + '\n') for i in environ['QUERY_STRING'].split('&')]
+    result = [bytes(i + '\n', 'utf8') for i in environ['QUERY_STRING'].split('&')]
     start_response(status, headers)
     return iter(result)
