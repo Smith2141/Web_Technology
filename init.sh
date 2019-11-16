bdab22000312f2a7a872a99274301698ed845960
Smith2141 <berez_max@mail.ru>
2019-11-16 09:46:58 +0000

    start jango project

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo rm /etc/nginx/sites-enabled/default 2> /dev/null
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test.conf
+sudo ln -s /home/box/web/etc/gunicorn.py   /etc/gunicorn.d/test.py
 #sudo rm /etc/gunicorn.d/default 2> /dev/null
 sudo /etc/init.d/gunicorn restart
 #sudo /etc/init.d/mysql start
