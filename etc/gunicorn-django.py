035411d8ea0e5f73a43d4d456162beb098dbea1a
smith_mint <berez_max@mail.ru>
2019-11-16 16:37:54 +0300

    django 3

--- etc/gunicorn-django.py
+++ etc/gunicorn-django.py
@@ -1,7 +1,7 @@
 CONFIG = {
     'mode': 'wsgi',
-    'working_dir': '/home/box/web/ask',
-    'pythonpath': '/home/box/web/ask',
+    'working_dir': '/home/box/web/ask/ask/',
+    'pythonpath': '/home/box/web/ask/ask/',
     #'python': '/usr/bin/python3',
     'args': (
         '--bind=0.0.0.0:8080',
--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo rm /etc/nginx/sites-enabled/default 2> /dev/null
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 #sudo ln -s /home/box/web/etc/gunicorn.py   /etc/gunicorn.d/test.py
 #sudo rm /etc/gunicorn.d/default 2> /dev/null
