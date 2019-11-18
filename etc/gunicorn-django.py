5695a2480cf81861b1f66225cce64648240708b2
Smith_work <berez_max@mail.ru>
2019-11-18 08:59:44 +0300

    django 13

--- etc/gunicorn-django.py
+++ etc/gunicorn-django.py
@@ -1,12 +1,12 @@
 CONFIG = {
     'mode': 'wsgi',
-    'working_dir': '/home/box/web/ask/ask/',
-    'pythonpath': '/home/box/web/ask/ask/',
+    'working_dir': '/home/box/web/ask/',
+    #'pythonpath': '/home/box/web/ask/',
     #'python': '/usr/bin/python3',
     'args': (
-        '--bind=0.0.0.0:8080',
+        '--bind=0.0.0.0:8000',
         '--workers=5',
         '--timeout=60',
-        'wsgi:application',
+        'wsgi',
     ),
 }
