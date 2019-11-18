35315e464fce0d2034cbbd3b1a25dcc95de2dae7
Smith2141 <berez_max@mail.ru>
2019-11-18 06:32:05 +0000

    GREATgit  add .!

--- etc/gunicorn-django.py
+++ etc/gunicorn-django.py
@@ -7,6 +7,6 @@ CONFIG = {
         '--bind=0.0.0.0:8000',
         '--workers=5',
         '--timeout=60',
-        'wsgi',
+        'wsgi:manage',
     ),
 }
--- init.sh
+++ init.sh
@@ -1,11 +1,11 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo rm /etc/nginx/sites-enabled/default 2> /dev/null
 sudo /etc/init.d/nginx restart
-gunicorn -b 0.0.0.0:8080 hello:application
+gunicorn -b 0.0.0.0:8000 hello:applicat
 #sudo ln -s /home/box/web/etc/gunicorn.py   /etc/gunicorn.d/test.py
 #sudo rm /etc/gunicorn.d/default 2> /dev/null
 #sudo /etc/init.d/mysql start
 
 #sudo gunicorn -c /home/box/web/etc/gunicorn.py hello:application
-sudo gunicorn -c /home/box/web/etc/gunicorn-django.py wsgi:application
+sudo gunicorn -c /home/box/web/etc/gunicorn-django.py wsgi:app123
 sudo /etc/init.d/gunicorn restart
